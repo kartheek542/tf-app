2d216efe902a32475b664f539c19db3548dc36a6
kartheek542 <kartheekdivvela@gmail.com>
2025-09-30 21:26:07 +0530

    Create key-pair and configured remote backed

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,13 @@ terraform {
         version = "~> 6.0"
       }
     }
+    backend "s3" {
+        bucket = "kar-terraform-apps-state"
+        key = "tf-elk-learning-state/terraform.tfstate"
+        region = "ap-south-1"
+        encrypt = true
+      
+    }
 }
 
 provider "aws" {
