aa90c712f56f2d80ddb2bee61ef8425e910933a8
kartheek542 <kartheekdivvela@gmail.com>
2025-10-01 22:13:07 +0530

    modularized network

--- main.tf
+++ main.tf
@@ -1,52 +1,7 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+module "dev-network" {
+  source = "git::https://github.com/kartheek542/tf-modules.git//aws-modules/elk-app/network?ref=main"
 
-  tags = {
-  }
-}
-
-resource "aws_subnet" "public_subnets" {
-  count      = var.subnets_count
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)
-
-  tags = {
-    subnet-type = "public"
-  }
-}
-
-resource "aws_subnet" "private_subnets" {
-  count      = var.subnets_count
-  vpc_id     = aws_vpc.main.id
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, var.subnets_count + count.index + 1)
-
-  tags = {
-    subnet-type = "private"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-  }
-}
-
-resource "aws_route_table" "app_route_table" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-  }
-}
-
-resource "aws_route" "ig_route" {
-  route_table_id         = aws_route_table.app_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route_table_association" "public_subnet_association" {
-  count          = var.subnets_count
-  subnet_id      = aws_subnet.public_subnets[count.index].id
-  route_table_id = aws_route_table.app_route_table.id
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
 }
