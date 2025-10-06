f097f9a5b755a261500cf48aa9b3c8843b22a0f2
kartheek542 <kartheekdivvela@gmail.com>
2025-10-06 08:22:10 +0530

    added database module, need to test it

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "app_server_public_ip" {
   value = module.app-server.public_ip
 }
+
+output "database_address" {
+  value = module.database.db_address
+}
+
+output "database_endpoint" {
+  value = module.database.db_endpoint
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,18 @@ variable "public_key_path" {
 variable "app_server_instance_type" {
   default = ""
 }
+variable "database_name" {
+  default = ""
+}
+variable "database_engine_version" {
+  default = ""
+}
+variable "database_port" {
+  default = 5432
+}
+variable "database_instance_type" {
+  default = ""
+}
+variable "database_master_username" {
+  default = ""
+}
