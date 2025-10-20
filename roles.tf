7cb456f1f877f418bc52918c6344595b49472fa8
kartheek542 <kartheekdivvela@gmail.com>
2025-10-20 18:14:24 +0530

    added eks access entires

--- roles.tf
+++ roles.tf
@@ -6,3 +6,10 @@ module "jump-server-iam-role" {
   eks_cluster_arns = [module.eks_cluster.cluster_arn]
   depends_on       = [module.eks_cluster]
 }
+
+module "eks_access" {
+  source = "git::https://github.com/kartheek542/tf-modules.git//aws-modules/elk-app/eks-access?ref=main"
+
+  cluster_name = module.eks_cluster.cluster_name
+  roles = [module.jump-server-iam-role.arn]
+}
